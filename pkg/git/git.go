1fc245addea6dc828f72773deb38ee6b25ad045b
Matthew Frisby <frisbm@gmail.com>
2023-08-01 12:01:28 -0500

    Update git.go

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -48,7 +48,7 @@ func Commit(dir string, file string, message string, user *User, branchName stri
 	if branchName != "" {
 		var branchRef *plumbing.Reference
 		// Check out the desired branch
-		branchRef, err = r.Reference(plumbing.ReferenceName("refs/heads/"+branchName), true)
+		branchRef, err = r.Reference(plumbing.NewRemoteReferenceName("origin", branchName), true)
 		if err != nil {
 			return fmt.Errorf("[Git] get branch reference: %v", err)
 		}
