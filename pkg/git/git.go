59b3b12e706c22c13049da845da32819dc9b84e2
Matthew Frisby <frisbm@gmail.com>
2023-10-23 14:24:34 -0500

    update deps

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -5,10 +5,10 @@ import (
 	"strings"
 	"time"
 
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/config"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 // Release Git release
--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -2,9 +2,9 @@ package parser
 
 import (
 	"fmt"
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"regexp"
 	"strings"
 )
